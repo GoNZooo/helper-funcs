dc01944c7b93f0f229b2a78fa2aee0ad20248d39
Rickard Andersson <rickard.m.andersson@gmail.com>
2013-09-08 23:05:51 +0200

    Merged auto-indent into helper-funcs.el

--- helper-funcs.el
+++ helper-funcs.el
@@ -1,8 +1,31 @@
 (defun add-auto-mode (extension prog-mode)
+  "Adds a binding for a prog-mode to a given file extension"
   (setq auto-mode-alist
 	(append
 	 (list
 	  (cons (concat "\\" extension "\\'") prog-mode))
 	 auto-mode-alist)))
+
+
+; List of languages we want to use auto-indent.
+(setq prog-mode-hooks '(emacs-lisp-mode-hook python-mode-hook))
+
+; Set up the binding to use as RET rebind.
+(global-set-key (kbd "C-j") 'newline-and-indent)
+
+; Define the config we want to use.
+(defun auto-indent-coding-config ()
+  (local-set-key (kbd "RET") (key-binding (kbd "C-j")))
+  (local-set-key (kbd "<S-return>") 'newline)
+  )
+
+(defun insert-auto-indent-hook (language-mode-hook)
+  "Adds auto-indentation to the selected prog-mode"
+   (add-hook language-mode-hook 'auto-indent-coding-config))
+
+; Add auto-indentation to the specified languages.
+; (mapc FUNCTION to apply to LIST)
+(defun add-auto-indentation ()
+  (mapc 'insert-auto-indent-hook prog-mode-hooks))
 
 (provide 'helper-funcs)
